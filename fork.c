d3aa077162f4ea7db2a14129a4a6d001b5382594
Jacob Feenstra <jacob.d.feenstra@gmail.com>
2025-01-10 19:20:34 -0800

    add wait to simulate non-deterministic processes!

--- fork.c
+++ fork.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main (int argc, char *argv[]) {
 	printf("hello world (pid:%d) \n", (int) getpid());
@@ -9,9 +10,10 @@ int main (int argc, char *argv[]) {
 		fprintf(stderr, "fork failed\n");
 		exit(1);
 	} else if (rc == 0) {   // child (new process)
-		printf("hello, I am child (pid:%d\n", (int) getpid());
+		printf("hello, I am child (pid:%d)\n", (int) getpid());
 	} else {   // parent goes down this path (main)
-		printf("hello, I am parent of %d (pid:%d)\n", rc, (int) getpid());
+		int wc = wait (NULL);
+		printf("hello, I am parent of %d (wc:%d) (pid:%d)\n", rc, wc, (int) getpid());
 	}
 	return 0;
 }
